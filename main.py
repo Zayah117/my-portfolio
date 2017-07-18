0588ea622fa83d5cee976b37c7cfd0d0ff2d2d92
Zayah117 <isaiah.baker@simpledove.com>
2017-07-18 12:26:43 -0400

    Read data from JSON file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for, flash
 
+import json
+
 app = Flask(__name__, static_url_path='/static/*')
+
+with open('data.json') as data_file:
+	data = json.load(data_file)
 
 @app.route('/')
 def front():
-	return render_template('front.html')
+	return render_template('front.html', data=data)
